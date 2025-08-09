0673fa52e13a3b9483c6376537b7d74a4c0faa39
Gabriel Carmo <63019523+GAB644@users.noreply.github.com>
2025-08-09 19:49:39 +0100

    0.1.5 IS OUT!

--- main.py
+++ main.py
@@ -1,27 +1,40 @@
 import tkinter as tk
-import random
 from tkinter import messagebox
+import random # AI RNG
 
-## Main Menu
+
+# Main Menu
 root = tk.Tk()
 root.title("RPS! Python")
+title_frame = tk.Frame(root)
+title_frame.pack()
 
-gamename_label = tk.Label(
-    root, 
-    text="RPS! Python", 
-    font=("arial", 12, "bold"), 
-    fg="blue"
+# Logo (
+blue_label = tk.Label(
+    title_frame, 
+    text="RPS!", 
+    font=("arial", 12, "italic"), 
+    fg="#336E9D"
 )
+yellow_label = tk.Label(
+    title_frame, 
+    text=" Python", 
+    font=("arial", 12, "bold"), 
+    fg="#D1AC36" # darker cuz #FFD643 is too bright for gray/white window backgrounds
+)
+blue_label.pack(side=tk.LEFT)
+yellow_label.pack(side=tk.LEFT)
+# )
+
 made_by_label = tk.Label(
-root,
-text="Made by GAB644",
-font=("Arial", 8, "normal")
+    root,
+    text="Made by GAB644",
+    font=("Arial", 8, "normal")
 )
 label = tk.Label(root, text="Ready to play?")
-gamename_label.pack()
 made_by_label.pack()
 label.pack()
-root.geometry("170x120")   
+root.geometry("170x120")
 
 # Choose Window
 def show_new_window():
@@ -33,6 +46,8 @@ def show_new_window():
 
     label = tk.Label(choose_window, text="Rock, Paper, or Scissors?")
     label.pack(pady=10)
+    
+    # Functions for choices
 # rock
     def choose_rock():
         choose_window.destroy()
@@ -63,12 +78,13 @@ def show_new_window():
 
         result_label = tk.Label(result_window, text=result, fg=result_color, font=("Arial", 12, "bold"))
         result_label.pack(pady=5)
-
+        
         back_button = tk.Button(result_window, text="Back to Menu", 
                                command=lambda: [result_window.destroy(), root.deiconify()])
         back_button.pack()
 
         result_window.protocol("WM_DELETE_WINDOW", root.destroy)
+        
 # paper
     def choose_paper():
         choose_window.destroy()
@@ -105,6 +121,7 @@ def show_new_window():
         back_button.pack()
 
         result_window.protocol("WM_DELETE_WINDOW", root.destroy)
+        
 # scissors
     def choose_scissors():
         choose_window.destroy()
@@ -158,9 +175,10 @@ new_window_button = tk.Button(root, text="Play", command=show_new_window)
 new_window_button.pack()
 
 
-exit_button = tk.Button(root, text="Nevermind", command=root.destroy)
+exit_button = tk.Button(root, text="Nevermind", 
+command=root.destroy)
 exit_button.pack()
 
-
+print("Working!")
 root.mainloop()
 # Code by GAB644 :]
